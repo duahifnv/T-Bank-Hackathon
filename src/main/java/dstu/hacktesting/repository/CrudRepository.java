aa352d6a44c9108717f2665dc3fa1707babceb2c
Максим Фоминцев <max135603@gmail.com>
2024-11-23 17:56:23 +0300

    Delete src/main/java/dstu/hacktesting directory

--- src/main/java/dstu/hacktesting/repository/CrudRepository.java
+++
@@ -1,8 +0,0 @@
-package dstu.hacktesting.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface CrudRepository<T> extends JpaRepository<T, Long> {
-}
--- src/main/java/dstu/hacktesting/repository/impl/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package dstu.hacktesting.repository.impl;
-
-import dstu.hacktesting.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
