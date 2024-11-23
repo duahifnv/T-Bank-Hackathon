626f5110f9b9bd788954e212aa74c6e7cb6bebf1
duahifnv <max135603@gmail.com>
2024-11-23 20:01:54 +0300

    Два новых поля

--- backend/src/main/java/org/hack/entity/User.java
+++ backend/src/main/java/org/hack/entity/User.java
@@ -21,4 +21,8 @@ public class User {
     private String login;
     @Column(name = "pass", nullable = false)
     private String password;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "surname", nullable = false)
+    private String surname;
 }
