94e77ede8d9681eaf1edc5a15fea6925b6138037
duahifnv <max135603@gmail.com>
2024-11-23 23:17:10 +0300

    Wallet контроллер

---
+++ backend/src/main/java/org/hack/controller/WalletController.java
@@ -0,0 +1,29 @@
+package org.hack.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.hack.dto.UserDto;
+import org.hack.dto.WalletDto;
+import org.hack.entity.User;
+import org.hack.entity.Wallet;
+import org.hack.mapper.WalletMapper;
+import org.hack.service.WalletService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/wallet")
+@RequiredArgsConstructor
+public class WalletController {
+    private final WalletService walletService;
+    @GetMapping()
+    public ResponseEntity<List<WalletDto>> getAllWallets() {
+        List<WalletDto> wallets = walletService.findAll();
+        return ResponseEntity.ok(wallets);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<WalletDto> getWalletByUserId(@PathVariable Long id) {
+        return ResponseEntity.ok().body(walletService.findByUserId(id));
+    }
+}
