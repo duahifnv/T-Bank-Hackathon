fc09ca46ea793051127e35f969dea854cd5f929a
Максим Фоминцев <max135603@gmail.com>
2025-01-21 04:58:53 +0300

    rebuild: Маппер транзакций

--- backend/src/main/java/org/hack/mapper/TransactionMapper.java
+++ backend/src/main/java/org/hack/mapper/TransactionMapper.java
@@ -1,18 +1,34 @@
 package org.hack.mapper;
 
-import org.hack.dto.TransactionDto;
+import org.hack.dto.request.TransactionRequest;
+import org.hack.dto.response.TransactionResponse;
 import org.hack.entity.Transaction;
+import org.hack.entity.Wallet;
+import org.hack.service.WalletService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDate;
 import java.util.List;
 
-@Mapper(componentModel = "spring",
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-public interface TransactionMapper {
-    Transaction dtoToModel(TransactionDto transactionDto);
-    @Mapping(source = "user.id", target = "userId")
-    TransactionDto modelToDto(Transaction transaction);
-    List<TransactionDto> toListDto(List<Transaction> transaction);
+@Mapper(componentModel = "spring", imports = LocalDate.class)
+public abstract class TransactionMapper {
+    @Autowired
+    protected WalletService walletService;
+    @Mapping(target = "date", expression = "java(LocalDate.now())")
+    @Mapping(source = "walletId", target = "wallet", qualifiedByName = "walletIdToWallet")
+    public abstract Transaction toTransaction(TransactionRequest transactionRequest);
+    @Mapping(source = "wallet", target = "walletId", qualifiedByName = "walletToWalletId")
+    public abstract TransactionResponse toTransactionResponse(Transaction transaction);
+    public abstract List<TransactionResponse> toTransactionResponse(List<Transaction> transactions);
+    @Named("walletIdToWallet")
+    protected Wallet walletIdToWallet(Long walletId) {
+        return walletService.findWalletById(walletId);
+    }
+    @Named("walletToWalletId")
+    protected Long walletToWalletId(Wallet wallet) {
+        return wallet.getWalletId();
+    }
 }
