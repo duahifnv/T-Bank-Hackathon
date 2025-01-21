1d29f1a43936d4b03af2e521d08a54fb3373c05c
Максим Фоминцев <max135603@gmail.com>
2025-01-21 04:59:08 +0300

    rebuild: Маппер кошельков

--- backend/src/main/java/org/hack/mapper/WalletMapper.java
+++ backend/src/main/java/org/hack/mapper/WalletMapper.java
@@ -1,27 +1,29 @@
 package org.hack.mapper;
 
-import org.hack.dto.WalletDto;
+import org.hack.dto.request.WalletRequest;
+import org.hack.dto.response.WalletResponse;
+import org.hack.entity.User;
 import org.hack.entity.Wallet;
+import org.hack.service.UserService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.List;
 
-// TODO: Адекватный маппинг
-@Mapper(componentModel = "spring")
-public interface WalletMapper {
-    @Mapping(source = "userId", target = "userId")
-    @Mapping(source = "total", target = "total")
-    Wallet dtoToModel(WalletDto walletDto);
-    @Mapping(source = "userId", target = "userId")
-    @Mapping(source = "total", target = "total")
-    WalletDto modelToDto(Wallet wallet);
-    @Mapping(source = "userId", target = "userId")
-    @Mapping(source = "total", target = "total")
-    List<WalletDto> toListDto(List<Wallet> wallets);
-    @Mapping(source = "userId", target = "userId")
-    @Mapping(source = "total", target = "total")
-    List<Wallet> toListModel(List<WalletDto> walletsDto);
+@Mapper(componentModel = "spring", imports = LocalDate.class)
+public abstract class WalletMapper {
+    @Autowired
+    protected UserService userService;
+    @Mapping(target = "issueDate", expression = "java(LocalDate.now())")
+    public abstract Wallet toWallet(WalletRequest walletRequest, User user);
+    @Mapping(source = "user", target = "userId", qualifiedByName = "userToUserId")
+    public abstract WalletResponse toWalletResponse(Wallet wallet);
+    public abstract List<WalletResponse> toWalletResponse(List<Wallet> wallets);
+    @Named("userToUserId")
+    protected Long userToUserId(User user) {
+        return user.getUserId();
+    }
 }
