2d41a14fe66d6d1d5185a323850b5aabef501361
Максим Фоминцев <max135603@gmail.com>
2025-01-21 04:58:40 +0300

    rebuild: Entity кошельков

--- backend/src/main/java/org/hack/entity/Wallet.java
+++ backend/src/main/java/org/hack/entity/Wallet.java
@@ -2,26 +2,38 @@ package org.hack.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
-@Table(name = "wallet")
+@Table(name = "wallets", schema = "public", catalog = "tbank_db")
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
-@Builder
 public class Wallet {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    @PrimaryKeyJoinColumn(name = "user_id")
-    private Long userId;
-    @Column(name = "total", nullable = false)
-    private BigDecimal total;
-    @OneToOne
-    @MapsId
-    @JoinColumn(name = "user_id")
+    @Column(name = "wallet_id")
+    private Long walletId;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
     private User user;
+    @Basic
+    @Column(name = "balance")
+    private BigDecimal balance;
+    @Basic
+    @Column(name = "name")
+    private String name;
+    @Basic
+    @Column(name = "issue_date")
+    private LocalDate issueDate;
+    @Basic
+    @Column(name = "last_update_date")
+    private LocalDate lastUpdateDate;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "wallet", cascade = CascadeType.ALL)
+    private List<Transaction> transactions;
 }
