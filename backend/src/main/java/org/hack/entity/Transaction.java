492affd251a741d590e8d7687658408e67abb75a
Максим Фоминцев <max135603@gmail.com>
2025-01-21 04:58:26 +0300

    rebuild: Entity транзакций

--- backend/src/main/java/org/hack/entity/Transaction.java
+++ backend/src/main/java/org/hack/entity/Transaction.java
@@ -2,35 +2,36 @@ package org.hack.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.Date;
+import java.time.LocalDate;
 
 @Entity
-@Table(name = "transactions")
+@Table(name = "transactions", schema = "public", catalog = "tbank_db")
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
-@Builder
 public class Transaction {
-    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     @Column(name = "transaction_id")
     private Long transactionId;
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-    @Column(name = "amount", nullable = false)
+    @Basic
+    @Column(name = "date")
+    private LocalDate date;
+    @Basic
+    @Column(name = "type_id")
+    private Integer typeId;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "wallet_id", referencedColumnName = "wallet_id")
+    private Wallet wallet;
+    @Basic
+    @Column(name = "amount")
     private BigDecimal amount;
-    @Column(name = "transaction_type", nullable = false)
-    private String transactionType;    // 'income' / 'expense'
-    @Column(name = "transaction_date", nullable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date transactionDate;
-    @Column(name = "category", nullable = false)
-    private String category;
+    @Basic
+    @Column(name = "category_id")
+    private Integer categoryId;
 }
+
