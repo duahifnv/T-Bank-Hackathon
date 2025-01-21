9adbc9a7dda2864c5291bdafc785e76d555a72e7
Максим Фоминцев <max135603@gmail.com>
2025-01-21 23:23:45 +0300

    refactor: Удалены лишние импорты

--- backend/src/main/java/org/hack/repository/WalletRepository.java
+++ backend/src/main/java/org/hack/repository/WalletRepository.java
@@ -1,16 +1,10 @@
 package org.hack.repository;
 
-import lombok.NonNull;
 import org.hack.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Repository
