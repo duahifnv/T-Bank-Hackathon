b478e84ec0b63a548272659b47d336a740bea3e5
Максим Фоминцев <max135603@gmail.com>
2025-01-21 04:59:01 +0300

    rebuild: Маппер пользователей

--- backend/src/main/java/org/hack/mapper/UserMapper.java
+++ backend/src/main/java/org/hack/mapper/UserMapper.java
@@ -1,26 +1,31 @@
 package org.hack.mapper;
 
-import org.hack.dto.UserDto;
+import org.hack.dto.request.RegistrationRequest;
+import org.hack.dto.response.UserResponse;
 import org.hack.entity.User;
+import org.hack.service.RoleService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Collections;
 import java.util.List;
 
-// TODO: Адекватный маппинг
-@Mapper(componentModel = "spring")
-public interface UserMapper {
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "surname", target = "surname")
-    User dtoToModel(UserDto userDto);
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "surname", target = "surname")
-    UserDto modelToDto(User user);
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "surname", target = "surname")
-    List<UserDto> toListDto(List<User> users);
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "surname", target = "surname")
-    List<User> toListModel(List<UserDto> users);
+@Mapper(componentModel = "spring", imports = Collections.class)
+public abstract class UserMapper {
+    @Autowired
+    protected PasswordEncoder passwordEncoder;
+    @Autowired
+    protected RoleService roleService;
+    public abstract UserResponse toUserResponse(User user);
+    public abstract List<UserResponse> toUserResponse(List<User> users);
+    @Mapping(target = "password", qualifiedByName = "getEncodedPassword")
+    @Mapping(target = "roles", expression = "java(Collections.singletonList(roleService.getUserRole()))")
+    public abstract User toUser(RegistrationRequest request);
+    @Named("getEncodedPassword")
+    protected String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
 }
